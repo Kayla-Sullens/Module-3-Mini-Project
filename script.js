687c29e32f8430a1213a208bd7f6735356f0bacb
Kayla <sullenspartyof3@gmail.com>
2023-07-16 12:55:01 -0700

    Finished

---
+++ script.js
@@ -0,0 +1,48 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var messageEL = document.querySelector("#message");
+
+var options = ["R", "P", "S"];
+
+function playGame() {
+    var userChoice = window.prompt("Enter R, P, or S:");
+
+    if(!userChoice) {
+        console.log('Game ended.');
+        messageEL.textContent = "The game was ended. Have a nice day!"
+        return;
+    }
+
+    userChoice = userChoice.toUpperCase();
+    var computerChoice = options[Math.floor(Math.random() * options.length)];
+
+    window.alert("The computer chose " + computerChoice);
+
+    if (userChoice === computerChoice) {
+        ties++;
+        window.alert("It's a tie!");
+    }   else if(
+        (userChoice ==="R" && computerChoice === "S") ||
+        (userChoice ==="P" && computerChoice === "R") ||
+        (userChoice ==="S" && computerChoice === "P") 
+    ) {
+        wins++;
+        window.alert("You win!");
+    }    else {
+            losses++;
+            window.alert("You lost!");
+        }
+
+        window.alert("Stats: Wins: " + wins + " Losses: " + losses + " Ties: " + ties);
+
+        var playAgain = window.confirm("Play again?");
+
+        if(playAgain) {
+            playGame();
+        }
+    
+        messageEL.textContent = "Thank you for playing!";
+}
+
+playGame();
